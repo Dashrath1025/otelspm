3e3f335462fb7d706bc6b75d53a15a1d012cf1c9
Dashrath1025 <dashrathkanzariya1025@gmail.com>
2024-05-23 15:05:49 +0530

    first  commit

---
+++ SPM/Controllers/Errors.cs
@@ -0,0 +1,17 @@
+﻿namespace SPM.Controllers
+{
+    public class Errors
+    {
+        public double ErrorRate { get; }
+
+        public Errors(double errorRate)
+        {
+            ErrorRate = errorRate;
+        }
+
+        public bool ProduceError()
+        {
+            return Random.Shared.NextDouble() > ErrorRate;
+        }
+    }
+}
---
+++ SPM/Controllers/HelloController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SPM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        private readonly Errors _errors;
+
+        public HelloController(Errors errors)
+        {
+            _errors = errors;
+        }
+
+        [HttpGet("message")]
+        public IActionResult GetMessage()
+        {
+            if (_errors.ProduceError())
+            {
+                return StatusCode(401);
+            }
+            return Ok("Hello from service");
+        }
+        [HttpGet("error")]
+        public IActionResult GenerateError()
+        {
+            try
+            {
+                // Simulate an error
+                throw new Exception("This is a simulated error");
+            }
+            catch (Exception ex)
+            {
+                //_errors.LogError(ex.Message);
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+    }
+}
